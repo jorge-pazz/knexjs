8938ad0b5fdf180932ad93916487d74d6199a67c
Jorge Alan <jorge.alann@hotmail.com>
2023-03-08 22:49:46 -0300

    Select com WHERE e com RAW

--- select.js
+++ select.js
@@ -8,3 +8,23 @@ knex.select("*")
     .catch((msgErro)=>{
         console.log(msgErro)
     })
+
+knex.select("*").where({id: 1}).table("produtos")
+    .then((produto)=>{
+        console.log(produto)
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro);
+    })    
+
+    //whereRaw
+    //WhereOr
+    //knex.raw
+
+    knex.raw("SELECT * FROM produtos WHERE id = 1")
+        .then((produto)=>{
+            console.log(produto[0])
+        })
+        .catch((msgErro)=>{
+            console.log(msgErro)
+        })
